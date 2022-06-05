26580e9bccc7850d4506cac4e843604d747401ca
Andrea Alvarado <a.alvarado1@ufl.edu>
2022-06-05 13:44:24 -0400

    adding var and keyboard event

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
+var previousWord = document.getElementById('previous-word')
+var incorrectLet = document.getElementById('incorrect-letters')
+var remainGuess = document.getElementById('remaining-guesses')
+var h2 = document.querySelector('h2')
+
+var wins = 0
+var losses = 0
 
 
 var words = [
@@ -14,8 +21,19 @@ var words = [
   'mango'
 ]
 
-var h2 = document.querySelector('h2')
 
+
+document.onkeyup = function(e){
+  //filter keypresses
+  //console.log(e.key) // access user's keypress
+  h2.textContent = e.key 
+  //pick a random word from words array
+
+  //compare picked and random word
+  //increment and show correct/incorrect
+  //show previous guess, correct/incorrect
+  //calculate score
+}
 
 
 
