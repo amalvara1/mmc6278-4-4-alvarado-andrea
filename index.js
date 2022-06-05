d803f823076e31c8bffc9f01b4b769143915b94a
Andrea Alvarado <a.alvarado1@ufl.edu>
2022-06-05 13:20:01 -0400

    reset js code due to issues

--- index.js
+++ index.js
@@ -1,11 +1,4 @@
 
-var previousWord = document.getElementById('previous-word')
-var incorrectLet = document.getElementById('incorrect-letters')
-var scoreEl = document.getElementById('score')
-var h2 = document.querySelector('h2')
-
-var wins = 0
-var losses = 0
 
 var words = [
   'bananas',
@@ -21,47 +14,9 @@ var words = [
   'mango'
 ]
 
-
-/*Select a word at random from "words" array*/
-var random_index = Math.floor(Math.random() * words.length);
-var randomWord = words[random_index];
-
-/*place it in word to guess element w/ letter replaced with underscores*/
-
-var wordToGuess = document.querySelector('h2')
-wordToGuess.textContent = (randomWord.replace(/[a-z]/g, '_'))
+var h2 = document.querySelector('h2')
 
 
-/*display 10 remaining guesses in remaining-guesses*/
-
-var remainingGuesses = document.getElementById('remaining-guesses');
-remainingGuesses.textContent = (10)
-
-
-document.onkeyup = function(e) {
-
-  h2.textContent = e.key
- 
-  var key = h2.textContent
-  if(key.includes(words, position)) {
-    //console.log(e.key)
-    randomWord.textContent = (key)
-  } 
-
-  var pickWord = words[Math.floor(Math.random() * words.length)]
-    
-  if (key === words) {
-    wins++
-    correctLet.textcontent = wins
-  } else {
-    losses++
-    incorrectLet.textContent = losses
-  }
-
-  previousWord.textContent = words
-  last
-
-}
 
 
 
