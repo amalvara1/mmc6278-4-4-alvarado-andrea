93e647deb5c07d74a45dccbc244dbc8f4c01e337
Andrea Alvarado <a.alvarado1@ufl.edu>
2022-06-05 19:22:02 -0400

    updated the js code

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
-var previousWord = document.getElementById('previous-word')
-var incorrectLet = document.getElementById('incorrect-letters')
-var remainGuess = document.getElementById('remaining-guesses')
-var h2 = document.querySelector('h2')
 
-var wins = 0
-var losses = 0
+var previousWord = document.getElementById('previous-word') 
+var incorrectLet = document.getElementById('incorrect-letters') 
+var remainGuess = document.getElementById('remaining-guesses') 
+var wordToGuess = document.querySelector('h2') 
 
-remainGuess.textContent = (10)
-
+ 
+var wins = 0 
+var losses = 0 
+remainGuess.textContent = (10) 
 
 var words = [
   'bananas',
@@ -23,33 +23,29 @@ var words = [
   'mango'
 ]
 
-/*Select a word at random from "words" array*/
-var random_index = Math.floor(Math.random() * words.length);
-var randomWord = words[random_index];
+
+/*Select a word at random from "words" array*/ 
+var rand = Math.floor(Math.random() * words.length); 
+var randomWord = words[rand]; 
+
+ 
+/*place it in word to guess element w/ letter replaced with underscores*/ 
+wordToGuess.textContent = (randomWord.replace(/[a-z]/g, '_')) 
 
 
-/*place it in word to guess element w/ letter replaced with underscores*/
+document.onkeyup = function(e) { 
 
-var wordToGuess = document.querySelector('h2')
-wordToGuess.textContent = (randomWord.replace(/[a-z]/g, '_'))
-
-
-document.onkeyup = function(e) {
-  //filter keypresses
-  var key = e.key.toLowerCase()
-  if(randomWord.includes(key)) {
-    wordToGuess.textContent = (randomWord.replace(/[a-z]/g, '_')).replace('_',(e.key))
-  } else {
-    losses++
-    incorrectLet.textContent+= '${e.key}'
-    remainGuess.textContent = (10 - losses)
-  }
-  //pick random word from array
-}
-
-
-
-
+//filter keypresses 
+var key = e.key.toLowerCase() 
+if(randomWord.includes(key)) { 
+wordToGuess.textContent = (randomWord.replace(/[a-z]/g, '_')).replace('_',(e.key)) 
+} else { 
+losses++ 
+incorrectLet.textContent+= (e.key) 
+remainGuess.textContent = (10 - losses) 
+} 
+ 
+} 
 
 
 
