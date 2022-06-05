77eb5044d2c8bb459c6a6fd763791371d3d66f79
Andrea Alvarado <a.alvarado1@ufl.edu>
2022-06-05 15:09:39 -0400

    added steps to pass initial setup

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ var h2 = document.querySelector('h2')
 var wins = 0
 var losses = 0
 
+remainGuess.textContent = (10)
+
 
 var words = [
   'bananas',
@@ -21,19 +23,32 @@ var words = [
   'mango'
 ]
 
+/*Select a word at random from "words" array*/
+var random_index = Math.floor(Math.random() * words.length);
+var randomWord = words[random_index];
 
 
-document.onkeyup = function(e){
+/*place it in word to guess element w/ letter replaced with underscores*/
+
+var wordToGuess = document.querySelector('h2')
+wordToGuess.textContent = (randomWord.replace(/[a-z]/g, '_'))
+
+
+document.onkeyup = function(e) {
   //filter keypresses
-  //console.log(e.key) // access user's keypress
-  h2.textContent = e.key 
-  //pick a random word from words array
-
-  //compare picked and random word
-  //increment and show correct/incorrect
-  //show previous guess, correct/incorrect
-  //calculate score
+  var key = e.key.toLowerCase()
+  if(randomWord.includes(key)) {
+    wordToGuess.textContent = (randomWord.replace(/[a-z]/g, '_')).replace('_',(e.key))
+  } else {
+    losses++
+    incorrectLet.textContent+= '${e.key}'
+    remainGuess.textContent = (10 - losses)
+  }
+  //pick random word from array
 }
+
+
+
 
 
 
