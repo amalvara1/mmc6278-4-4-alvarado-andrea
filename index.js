3b928a33cd714abf5fd63fbc49deaec50ad16641
Andrea Alvarado <a.alvarado1@ufl.edu>
2022-06-05 22:24:43 -0400

    removed irrelevant code

--- index.js
+++ index.js
@@ -1,14 +1,4 @@
 
-var previousWord = document.getElementById('previous-word') 
-var incorrectLet = document.getElementById('incorrect-letters') 
-var remainGuess = document.getElementById('remaining-guesses') 
-var wordToGuess = document.querySelector('h2') 
-
- 
-var wins = 0 
-var losses = 0 
-remainGuess.textContent = (10) 
-
 var words = [
   'bananas',
   'grapes',
@@ -21,33 +11,42 @@ var words = [
   'meatloaf',
   'ukulele',
   'mango'
-]
-
-
-/*Select a word at random from "words" array*/ 
-var rand = Math.floor(Math.random() * words.length); 
-var randomWord = words[rand]; 
+]; 
 
  
-/*place it in word to guess element w/ letter replaced with underscores*/ 
-wordToGuess.textContent = (randomWord.replace(/[a-z]/g, '_')) 
+var previousWord = document.getElementById('previous-word')  
+var incorrectLet = document.getElementById('incorrect-letters')  
+var remainGuess = document.getElementById('remaining-guesses')  
+var wordToGuess = document.querySelector('h2')  
 
 
-document.onkeyup = function(e) { 
+remainGuess.textContent = (10)  
+var wins = 0;  
+var losses = 0; 
 
-//filter keypresses 
+
+ 
+/*Select a word at random from "words" array*/  
+var rand = Math.floor(Math.random() * words.length);  
+var randomWord = words[rand];  
+
+ 
+/*place it in word to guess element w/ letter replaced with underscores*/  
+wordToGuess.textContent = (randomWord.replace(/[a-z]/g, '_'))  
+
+
+document.onkeyup = function(e) {  
+//filter keypresses  
 var key = e.key.toLowerCase() 
-if(randomWord.includes(key)) { 
-wordToGuess.textContent = (randomWord.replace(/[a-z]/g, '_')).replace('_',(e.key)) 
-} else { 
-losses++ 
-incorrectLet.textContent+= (e.key) 
-remainGuess.textContent = (10 - losses) 
-} 
- 
-} 
 
 
+if(randomWord.includes(key)) {  
+wordToGuess.textContent = (randomWord.replace(/[a-z]/g, '_')).replace('_',(e.key))  
+} else {  
+losses++  
+incorrectLet.textContent+= (e.key)  
+remainGuess.textContent = (10 - losses)
+}  
 
 
-
+}  
